768b56c08bd63867560ff07e0232b9834832aa13
Rafael Barbosa <rafael.barbosa@beblue.com.br>
2019-02-14 11:00:24 -0200

    add datadog changes.

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,8 @@ export AWS_HOST_IP=$(curl -sf -m 3 http://169.254.169.254/latest/meta-data/local
 
 [ ! -z "$final_host_ip" ] && echo "$final_host_ip host-ip" >> /etc/hosts
 
+export HOST_IP="$final_host_ip"
+
 # Run Consul Register Control
 # python /logger-service/consul_script.py
 
